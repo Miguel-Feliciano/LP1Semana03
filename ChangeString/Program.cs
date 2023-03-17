7e44dc7e16cd81f44751d38acae362a56542c99b
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-17 00:28:50 +0000

    Completed exercise 1

---
+++ ChangeString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string iS;
+            char charToRemove;
+            string oS = "";
+
+            Console.WriteLine("Enter a string:");
+            iS = Console.ReadLine();
+
+            Console.WriteLine("Enter a character to remove:");
+            charToRemove = (char)Console.Read();
+
+
+            foreach (char c in iS)
+            {
+                if (c != charToRemove)
+                {
+                    oS += c;
+                }
+            }
+
+            Console.WriteLine($"The string without the character '{charToRemove}' is: {oS}");
+        }
+    }
+
+}
+
